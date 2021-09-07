85f8de1a6703100dbb82d7d625f374059da8210e
Brandon Tassone <brandontassone@gmail.com>
2021-09-07 16:20:25 -0400

    Changed the list route from the proxy to the new security groups handler. Added a SecurityGroupListHandler function which calls the new ListSecurityGroups function in the security.go file.

---
+++ api/handlers_security.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"fmt"
+	"github.com/YaleSpinup/apierror"
+	"github.com/YaleSpinup/ec2-api/ec2"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
+
+func (s *server) SecurityGroupListHandler(w http.ResponseWriter, r *http.Request) {
+	w = LogWriter{w}
+	vars := mux.Vars(r)
+	account := s.mapAccountNumber(vars["account"])
+	name := vars["name"]
+
+	role := fmt.Sprintf("arn:aws:iam::%s:role/%s", account, s.session.RoleName)
+
+	session, err := s.assumeRole(
+		r.Context(),
+		s.session.ExternalID,
+		role,
+		"",
+		"arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess",
+	)
+	if err != nil {
+		msg := fmt.Sprintf("failed to assume role in account: %s", account)
+		handleError(w, apierror.New(apierror.ErrForbidden, msg, err))
+		return
+	}
+
+	service := ec2.New(
+		ec2.WithSession(session.Session),
+		ec2.WithOrg(s.org),
+	)
+
+	out, err := service.ListSecurityGroups(r.Context(), "", name)
+	if err != nil {
+		handleError(w, err)
+		return
+	}
+
+	w.Header().Set("X-Items", strconv.Itoa(len(out)))
+
+	handleResponseOk(w, out)
+}
---
+++ ec2/security.go
@@ -0,0 +1,38 @@
+package ec2
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	log "github.com/sirupsen/logrus"
+)
+
+func (e *Ec2) ListSecurityGroups(ctx context.Context, org string, name string) ([]map[string]*string, error) {
+	log.Infof("listing ec2 security groups (name: '%s', org: '%s')", name, org)
+
+	var filters []*ec2.Filter
+	if org != "" {
+		filters = []*ec2.Filter{inOrg(org)}
+	}
+
+	input := ec2.DescribeSecurityGroupsInput{
+		GroupIds: aws.StringSlice([]string{}),
+		Filters:  filters,
+	}
+
+	out, err := e.Service.DescribeSecurityGroupsWithContext(ctx, &input)
+	if err != nil {
+		return nil, ErrCode("listing security groups", err)
+	}
+
+	log.Debugf("returning list of %d snapshots", len(out.SecurityGroups))
+
+	list := make([]map[string]*string, len(out.SecurityGroups))
+	for i, s := range out.SecurityGroups {
+		list[i] = map[string]*string{
+			"id": s.GroupId,
+		}
+	}
+
+	return list, nil
+}
