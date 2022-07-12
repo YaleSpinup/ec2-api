5323a6b122b73f95a1e55718821e765f0d5ed84d
nvnyale <naveen.akula@yale.edu>
2022-07-12 15:14:31 -0400

    removed comments

--- iam/iam.go
+++ iam/iam.go
@@ -11,15 +11,11 @@ import (
 type Iam struct {
 	session *session.Session
 	Service iamiface.IAMAPI
-	// DefaultKMSKeyId string
-	// DefaultSgs      []string
-	// DefaultSubnets  []string
-	// org             string
 }
 
 type IAMOption func(*Iam)
 
-// New creates a new Ec2
+// New creates a new Iam
 func New(opts ...IAMOption) *Iam {
 	i := Iam{}
 
@@ -40,42 +36,3 @@ func WithSession(sess *session.Session) IAMOption {
 		e.session = sess
 	}
 }
-
-// func WithCredentials(key, secret, token, region string) IAMOption {
-// 	return func(e *Iam) {
-// 		log.Debugf("creating new session with key id %s in region %s", key, region)
-// 		sess := session.Must(session.NewSession(&aws.Config{
-// 			Credentials: credentials.NewStaticCredentials(key, secret, token),
-// 			Region:      aws.String(region),
-// 		}))
-// 		e.session = sess
-// 	}
-// }
-
-// func WithDefaultKMSKeyId(keyId string) IAMOption {
-// 	return func(e *Iam) {
-// 		log.Debugf("setting default kms keyid %s", keyId)
-// 		e.DefaultKMSKeyId = keyId
-// 	}
-// }
-
-// func WithDefaultSgs(sgs []string) IAMOption {
-// 	return func(e *Iam) {
-// 		log.Debugf("setting default security groups %+v", sgs)
-// 		e.DefaultSgs = sgs
-// 	}
-// }
-
-// func WithDefaultSubnets(subnets []string) IAMOption {
-// 	return func(e *Iam) {
-// 		log.Debugf("setting default subnets %+v", subnets)
-// 		e.DefaultSubnets = subnets
-// 	}
-// }
-
-// func WithOrg(org string) IAMOption {
-// 	return func(e *Iam) {
-// 		log.Debugf("setting org to %s", org)
-// 		e.org = org
-// 	}
-// }
